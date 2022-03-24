6f146cf827436fb84437d9e8909a352a9ac9db79
liangqiding <742740345@qq.com>
2022-03-24 23:23:20 +0800

    init project

---
+++ m-mqtt-broker/src/main/java/m/mqtt/broker/MqttBrokerApplication.java
@@ -0,0 +1,15 @@
+package m.mqtt.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author lqd
+ */
+@SpringBootApplication
+public class MqttBrokerApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(MqttBrokerApplication.class, args);
+    }
+
+}
